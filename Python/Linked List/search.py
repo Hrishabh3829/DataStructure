f4e534a0bb825d15dc14012c357fecb87052e743
Hrishabh Gupta <113886587+Hrishabh3829@users.noreply.github.com>
2025-07-30 19:02:27 +0000

    search gives the index in LL

---
+++ Python/Linked List/search.py
@@ -0,0 +1,144 @@
+class Node:
+  def __init__(self,value):
+    self.data=value
+    self.next=None
+
+
+class LinkedList:
+  def __init__(self):
+    #empty linked list
+    self.head=None
+    #No .of Nodes in the LL
+    self.n=0
+
+
+  def __len__(self):
+    return self.n
+
+  def insert_head(self,value):
+
+    #new node
+    new_node=Node(value)
+
+    #create connection
+    new_node.next= self.head
+
+    #reassign head
+    self.head=new_node
+
+    #increment n
+    self.n=self.n + 1
+
+  def __str__(self):
+    curr = self.head
+
+    result=''
+
+    while curr != None:
+      result=result + str(curr.data) + '->'
+      curr =curr.next
+    return result[:-2]
+
+  def append(self,value):
+
+    new_node = Node(value)
+
+    if self.head==None:
+      #empty
+      self.head=new_node
+      self.n=self.n+1
+      return 
+
+    curr = self.head
+    while curr.next !=None:
+      curr =curr.next
+
+    #you are at the last node
+    curr.next = new_node
+    self.n=self.n+1
+
+  def insert_after(self,after,value):
+    new_node = Node(value)
+    curr=self.head
+    while curr!=None:
+      if curr.data== after:
+        break
+      curr =curr.next
+      self.n = self.n + 1
+    if curr!=None:
+      new_node.next=curr.next
+      curr.next=new_node
+    else:
+      return 'Item not found'
+
+  def clear(self):
+    self.head=None
+    self.n = 0
+
+  def delete_head(self):
+    if self.head==None:
+      #empty
+      return 'Empty LL'
+    self.head=self.head.next
+    self.n=self.n -1 
+
+
+  def pop(self):
+    if self.head==None:
+      return 'Empty LL'
+
+    curr = self.head
+
+    #check linked list mai 1 item hai?
+    if curr.next==None:
+      #head hi hoga (delete from head)
+      return self.delete_head()
+
+    while curr.next.next != None:
+      curr=curr.next
+    #curr --> 2nd last node 
+    curr.next = None
+    self.n = self.n-1
+
+  def remove(self, value):
+          # Case 1: Empty list
+    if self.head is None:
+      return 'Empty Linked List'
+
+      # Case 2: Head needs to be removed
+    if self.head.data == value:
+      self.head = self.head.next
+      self.n -= 1
+      return
+
+          # Case 3: Value in the middle or end
+    curr = self.head
+    while curr.next is not None:
+      if curr.next.data == value:
+        curr.next = curr.next.next
+        self.n -= 1
+        return
+      curr = curr.next
+
+    return 'Not Found'
+
+  def search(self,item):
+    curr=self.head
+    pos=0
+    while curr != None:
+      if curr.data == item:
+        return pos
+      curr=curr.next
+      pos=pos+1
+
+    return 'Not found'
+
+
+L=LinkedList()
+L.insert_head(1)
+L.insert_head(2)
+L.insert_head(3)
+L.insert_head(4)
+
+
+print(L.search(3))
