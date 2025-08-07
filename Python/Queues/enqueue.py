dfded2375c2faac9f0dd1f26df8edd0ad9231d58
Hrishabh Gupta <113886587+Hrishabh3829@users.noreply.github.com>
2025-08-07 18:46:08 +0000

    Enqueue in LL

---
+++ Python/Queues/enqueue.py
@@ -0,0 +1,26 @@
+class Node:
+
+  def __init__(self, value):
+    self.data = value
+    self.next = None
+
+
+class Queue:
+
+  def __init__(self):
+    self.front = None
+    self.rear = None
+
+  def enqueue(self, value):
+    new_node = Node(value)
+    if self.rear == None:
+      self.front = new_node
+      self.rear = self.front
+    else:
+      self.rear.next = new_node
+      self.rear = new_node
+    return value
+
+s=Queue()
+
+print(s.enqueue(21))
