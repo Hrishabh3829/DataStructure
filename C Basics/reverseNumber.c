513208330138d9a5cdb6446efacfe50d5bb2c96a
Hrishabh Gupta <113886587+Hrishabh3829@users.noreply.github.com>
2025-11-07 01:20:10 +0530

    Reverse a number

---
+++ C Basics/reverseNumber.c
@@ -0,0 +1,16 @@
+// 123
+// 321
+
+#include<stdio.h>
+int main(){
+    int n;
+    scanf("%d",&n);
+    int rev=0,rem;
+    while(n>0){
+        rem=n%10;
+        rev=rev*10+rem;
+        n=n/10;
+    }
+    printf("%d",rev);
+}
+
