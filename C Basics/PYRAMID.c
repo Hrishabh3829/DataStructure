0384f8645bf35d45564ae916ac3dfa779308101a
Hrishabh Gupta <113886587+Hrishabh3829@users.noreply.github.com>
2025-12-17 15:27:37 +0530

    Pyramid Patterns

--- C Basics/PYRAMID.c
+++ C Basics/PYRAMID.c
@@ -17,3 +17,23 @@ int main(){
     }
     return 0;
 }
+
+// 4
+// * * * *
+// * * *
+// * *
+// *
+#include<stdio.h>
+int main(){
+    int n;
+    int i ,j,rows;
+    scanf("%d",&rows);
+    
+    for(i=rows;i>=1;i--){
+        for(j=1;j<=i;j++){
+            printf("*");
+        }
+        printf("\n");
+    }
+    return 0;
+}
